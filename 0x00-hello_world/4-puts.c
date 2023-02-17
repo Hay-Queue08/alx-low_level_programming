91db9adca12cddd7ede96ca7f02087f7f24c4b40
Hay-Queue08 <amosun.a.o@gmail.com>
2023-02-16 20:31:40 -0800

    puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A program that prints exactly like put
+ * Return: o(if sucess)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
