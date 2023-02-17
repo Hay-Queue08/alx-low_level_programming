f3f6c3fecbf928a0bccf155aaa27c9769659c7b0
Hay-Queue08 <amosun.a.o@gmail.com>
2023-02-16 20:44:54 -0800

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * program that prints exactly c usinh the printf function
+ * Return: 0 (if sucess)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
