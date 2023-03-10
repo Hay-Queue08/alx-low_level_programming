5686441095568bb75ff771fb68307f8dd13f6c63
Hay-Queue08 <amosun.a.o@gmail.com>
2023-03-10 10:48:36 -0800

    0x0A-argc_argv

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * main - adds two positive numbers
+ * @argc: argument count
+ * @argv: array of arguments
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int sum;
+	int num;
+	int arg_index;
+	int char_index;
+
+	sum = 0;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+	for (arg_index = 1; arg_index < argc; arg_index++)
+	{
+		for (char_index = 0; argv[arg_index][char_index] != '\0'; char_index++)
+		{
+			if (!isdigit(argv[arg_index][char_index]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		num = atoi(argv[arg_index]);
+
+		if (num < 0)
+		{
+			printf("Error\n");
+			return (1);
+		}
+		sum += num;
+
+	}
+	printf("%d\n", sum);
+
+	return (0);
+}
