ff2374170d9ac757186cfdb3f18c5b1699e2cdcf
Hay-Queue08 <amosun.a.o@gmail.com>
2023-03-17 14:17:39 -0700

    0x0C-more_malloc_free

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -5,7 +5,7 @@
  * *_realloc - reallocates a memory block using malloc and free
  * @ptr: pointer to the memory previously allocated by malloc
  * @old_size: size of the allocated memory for ptr
- * @mew_size: new size of the new memory block
+ * @new_size: new size of the new memory block
  *
  * Return: pointer to the newly allocated memory block
  */
