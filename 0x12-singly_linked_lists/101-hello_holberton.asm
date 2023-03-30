79ac5ae92a2ae8f74a170a0ff968d89d6db0a262
Hay-Queue08 <amosun.a.o@gmail.com>
2023-03-30 09:50:07 -0700

    0x12-singly_linked_lists

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -6,14 +6,14 @@ global main
 main:
 push rbp
 
-mov rdi, fmt
-mov rsi, msg
-mov rax, 0
+mov rdi,fmt
+mov rsi,msg
+mov rax,0
 call printf
 
-poprbp
+pop rbp
 
-mov rax, 0
+mov rax,0
 ret
 
 section .data
