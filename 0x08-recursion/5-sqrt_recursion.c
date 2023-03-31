305bcd2e9bf7cfd50fb46e63293e50094630f8c4
Hay-Queue08 <amosun.a.o@gmail.com>
2023-03-31 02:35:16 -0700

    0x08-recursion

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,16 +1,17 @@
 #include "main.h"
 
-int actual_sqrt_recursion(int n, int i)
+int actual_sqrt_recursion(int n, int i);
 
 /**
- * _sqrt_recursion - returns the natural square root of a number.
- * @n: number to calculate the square root of
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: number to calculate the square root of
  *
- * Return: the resultimg square root
+ * Return: the resulting square root
  */
-int _sqrt_recursion(int n)
+
+int _sqrt_recursion(int n)
 {
-	if (n < 0)
+	if (n < 0)
 		return (-1);
 	return (actual_sqrt_recursion(n, 0));
 }
@@ -18,11 +19,12 @@ int _sqrt_recursion(int n)
 /**
  * actual_sqrt_recursion - recurses to find the natural
  * square root of a number
- * @n: number to calculate the square root of
+ * @n: number to calculate the sqaure root of
  * @i: iterator
  *
- * Return: the resultimg square root
+ * Return: the resulting square root
  */
+
 int actual_sqrt_recursion(int n, int i)
 {
 	if (i * i > n)
--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,6 +1,6 @@
 #include "main.h"
 
-int actual_prime(int n, int i);
+int actual_prime(int n, int i);
 
 /**
  * is_prime_number - says if an integer is a prime number or not
@@ -8,6 +8,7 @@ int actual_prime(int n, int i);
  *
  * Return: 1 if n is a prime number, 0 if not
  */
+
 int is_prime_number(int n)
 {
 	if (n <= 1)
@@ -16,13 +17,14 @@ int is_prime_number(int n)
 }
 
 /**
- * actual_prime - calculates if a number is a prime recursively
+ * actual_prime - calculates if a number is prime recursively
  * @n: number to evaluate
- * @i: interator
+ * @i: iterator
  *
  * Return: 1 if n is prime, 0 if not
  */
-int actual_prime(int n, int i)
+
+int actual_prime(int n, int i)
 {
 	if (i == 1)
 		return (1);
